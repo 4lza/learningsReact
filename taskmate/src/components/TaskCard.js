2ec7bbfbe47d1d250398f87fb95e5b4fc9b0acab
4lza <aleezaahmed315@gmail.com>
2024-05-05 14:04:46 +0100

    section 4 of course

---
+++ taskmate/src/components/TaskCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TaskCard = ({task, handleDelete}) => {
+  return (
+    <div>
+        <li className={task.completed ? "completed" : "incomplete"}>
+                    <span>{task.id} - {task.name}</span>
+                    <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
+                </li>
+    </div>
+  )
+}
--- taskmate/src/components/TaskList.js
+++ taskmate/src/components/TaskList.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { TaskCard } from './TaskCard';
+import { BoxCard } from './BoxCard';
 
 export const TaskList = () => {
     const [tasks, setTasks] = useState([
@@ -18,12 +20,18 @@ export const TaskList = () => {
         <ul>
             <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
             { show && tasks.map((task) => (
-                <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
-                    <span>{task.id} - {task.name}</span>
-                    <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
-                </li>
+                <TaskCard key={task.id} task={task} handleDelete={handleDelete} /> 
             )) }
         </ul>
+        <BoxCard result='success'>
+            <p className="title">Lorem ipsum dolor sit amet.</p>
+            <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, adipisci.</p>
+        </BoxCard>
+        <BoxCard result='warning'>
+            <p className="title">Lorem ipsum dolor sit amet.</p>
+            <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, asperiores?</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </BoxCard>
     </>
   )
 }
